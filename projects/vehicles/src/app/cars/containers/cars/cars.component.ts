db580d222fc721bb87ee690d887d39405a94654a
kaplan81 <gesteira.pro@gmail.com>
2024-06-30 12:29:49 +0200

    remove routerlink

--- projects/vehicles/src/app/cars/containers/cars/cars.component.ts
+++ projects/vehicles/src/app/cars/containers/cars/cars.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { RoutedComponents } from '@vehicles/app/enums';
 import { TitleMixin, emptyBase } from '@vehicles/app/mixins';
 import { VehiclesService } from '@vehicles/cars/services/vehicles.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink],
+  imports: [],
   standalone: true,
   styleUrl: './cars.component.scss',
   templateUrl: './cars.component.html',
